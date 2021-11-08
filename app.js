73a43186bdb0d244557e230e1a918584bdaae81b
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-08 23:44:49 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+// template engine
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -8,4 +13,4 @@ app.listen(port, () => {
 
 require('./routes')(app)
 
-module.exports = app
+module.exports = app  //app測試環境用
