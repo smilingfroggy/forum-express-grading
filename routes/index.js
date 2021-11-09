ed1ae1ed4bd9e0c07017e9b53d3b55addb441a69
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-10 00:53:50 +0800

    Add isAdmin to users table and authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,28 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/', (req, res) => { res.redirect('/restaurants') })
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')  //回到一般使用者首頁
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/', authenticated, (req, res) => { res.redirect('/restaurants') })
 
-  app.get('/admin', (req, res) => { res.redirect('/admin/restaurants') })
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => { res.redirect('/admin/restaurants') })
+
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
 
