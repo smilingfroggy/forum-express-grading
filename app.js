35415b878c3375d1da2c5b9784b71dfb65620a3b
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-09 16:55:37 +0800

    feat: Confirm user password and unique email; install session, connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -9,6 +11,20 @@ app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extented: true }))
+
+// setup session and flash message
+app.use(session({
+  secret: 'MySecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,15 +7,34 @@ const userController = {
     res.render('signup')
   },
   signUp: (req, res) => {
-    console.log(req.body)
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
-      .then(user => {
-        return res.redirect('/signin')
-      })
+    const { name, email, password, passwordCheck } = req.body
+    console.log('Got req.body: ', name, email, password, passwordCheck)
+    // confirm password
+    if (password !== passwordCheck) {
+      req.flash('error_messages', '兩次輸入密碼不同！')
+      console.log('兩次輸入密碼不同！')
+      return res.redirect('/signup') //OK
+      // return res.render('signup', { name, email, password })  // flash訊息會在下次重新整理才出現；saveUninitialized: true/false都一樣
+
+    } else {
+      User.findOne({ where: { email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_messages', '此帳號已註冊過')
+            console.log('此帳號已註冊過')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+              name, email,
+              password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+            })
+              .then(user => {
+                req.flash('success_message', '成功註冊帳號！')
+                return res.redirect('/signin')
+              })
+          }
+        })
+    }
   }
 }
 
