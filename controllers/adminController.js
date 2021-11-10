807ba7d4c48f1cf4d3128883959ebb57532efb6a
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-10 15:56:01 +0800

    Update admin create page for editing usage

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -35,6 +35,12 @@ const adminController = {
         return res.render('admin/restaurant', { restaurant })
       })
   },
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true })
+      .then(restaurant => {
+        return res.render('admin/create', { restaurant })
+      })
+  },
 }
 
 module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -22,18 +22,22 @@ module.exports = (app, passport) => {
 
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
+  // 管理員後台 CRUD
   app.get('/admin', authenticatedAdmin, (req, res) => { res.redirect('/admin/restaurants') })
-
+  // read all
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
-
-  app.get('/admin/restaurants/:id', adminController.getRestaurant)
-
+  // create
   app.get('/admin/restaurants/create', authenticatedAdmin,
     adminController.createRestaurant)
 
   app.post('/admin/restaurants', authenticatedAdmin,
     adminController.postRestaurant)
+  // read one
+  app.get('/admin/restaurants/:id', authenticatedAdmin,adminController.getRestaurant)
+  // edit one
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
 
+  // 註冊、登入、登出
   app.get('/signup', userController.signUpPage)
 
   app.post('/signup', userController.signUp)
