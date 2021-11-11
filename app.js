8b2b9d03e7dcb99227929371aa8a0d1570e2f62d
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-11 16:08:54 +0800

    Listen to process.env.PORT and define Profile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // template engine
 app.engine('handlebars', handlebars())
