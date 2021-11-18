d6169f25a5f7a2d8b93f3257221c23b591db4a29
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-18 17:08:35 +0800

    Show user comments at profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require("../models")
 const User = db.User
+const Restaurant = db.Restaurant
+const Comment = db.Comment
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
@@ -52,9 +54,12 @@ const userController = {
   },
   getUser: (req, res) => {
     // console.log('start userController.getUser')
-    return User.findByPk(req.params.id)
+    return User.findByPk(req.params.id, {
+      include: { model: Comment, include: [Restaurant] }
+    })
       .then(user => {
         // console.log('userController.getUser..after then', user)
+        // console.log('user.Comment', user.Comments)
         return res.render('profile', { user: user.toJSON() })
       })
   },
