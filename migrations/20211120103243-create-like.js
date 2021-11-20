5bbae6e69670951a93834ac9de84916297a58503
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-20 18:46:32 +0800

    Create like model and set association between restaurant and user

---
+++ migrations/20211120103243-create-like.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'restaurants', key: 'id' }
+      },
+      RestaurantId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Likes');
+  }
+};
--- models/user.js
+++ models/user.js
@@ -17,6 +17,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'UserId',
         as: 'FavoritedRestaurants'
       })
+      User.belongsToMany(models.Restaurant, {
+        through: models.Favorite,
+        foreignKey: "UserId",
+        as: 'LikedRestaurants'
+      })
     }
   };
   User.init({
