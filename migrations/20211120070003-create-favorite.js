b9adcb3af5db15e00c104b70aaa9203ea0e29c73
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-20 15:18:23 +0800

    Create favorite model and set association between restaurant and user

---
+++ migrations/20211120070003-create-favorite.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      RestaurantId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'restaurants', key: 'id' }
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Favorites');
+  }
+};
