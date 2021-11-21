c2cb8d532ddb7076d98f8ef6b04be2b478176e8d
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-21 17:51:05 +0800

    Creat followship model and add follower and following to Users

--- config/passport.js
+++ config/passport.js
@@ -30,7 +30,10 @@ passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
       { model: Restaurant, as: 'FavoritedRestaurants' },
-      { model: Restaurant, as: 'LikedRestaurants' }]
+      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followings' },
+      { model: User, as: 'Followers' }
+    ]
   })
     .then(user => {
       user = user.toJSON()
