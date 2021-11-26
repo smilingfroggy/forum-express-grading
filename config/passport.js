a3c317d9bd1718d08217a9d6edb1f6497f9a283a
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-27 03:02:56 +0800

    Add authenticated and authenticatedAdmin middleware

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,11 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
@@ -21,6 +27,26 @@ passport.use(new LocalStrategy({
       return cb(null, user)
     })
 }))
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+jwtOptions.secretOrKey = process.env.JWT_SECRET
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  User.findByPk( jwt_payload.id, {
+    include: [
+      { model: db.Restaurant, as: 'FavoritedRestaurants'},
+      { model: db.Restaurant, as: 'LikedRestaurants'},
+      { model: User, as: 'Followers'},
+      { model: User, as: 'Followings' }
+    ]
+  }).then(user => {
+    if (!user) return next(null, false)
+    return next(null, user)
+  })
+})
+
+passport.use(strategy)
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
